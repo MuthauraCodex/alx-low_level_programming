3de2efde697b66fb46718ded417f71309e561e51
MuthauraCodex <gwenivymuthaura@gmail.com>
2022-09-12 03:54:22 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 
 /**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
-
+* main - print the string in the put function
+* Description: using the main function
+* Return: 0 if success.
+*/
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }

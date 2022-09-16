95e9a288f50a7681e72738206a6612fbca6e8673
MuthauraCodex <gwenivymuthaura@gmail.com>
2022-09-16 08:38:25 -0700

    Print alphabet letters in lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INLCUDED
+#ifndef main_h
+#define main_h
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,7 +13,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
-void print_time_table(int n);
+void print_times_table(int n);
 void num(int r, int n);
 
 #endif
